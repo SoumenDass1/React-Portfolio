f773bc072864ed643ae600b6c8af75515d2e3b20
SoumenDass2006 <soumendass1357@gmail.com>
2025-09-03 13:10:39 +0530

    Header id done. Now, working on home page

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,19 +1,42 @@
-import React from 'react';
-import '../css/profile.css';
+import React from "react";
+import "../css/profile.css";
 
-function profile() {
+function Profile() {
   return (
     <>
-    <header>
-        <div className='container'>
-            <div className="logo-box">
-                <h1 className="logo">Soumen Dass</h1>
-            </div>
+      <header>
+        <div className="container">
+          <div className="logo-box">
+            <h1 className="logo">Portfolio</h1>
+          </div>
 
+          <nav className="nav-bar">
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/about">About</a></li>
+              <li><a href="/skills">Skills</a></li>
+              <li><a href="/projects">Projects</a></li>
+              <li><a href="/contact">Contact</a></li>
+            </ul>
+          </nav>
         </div>
-    </header>
+      </header>
+
+      <main>
+        <section className="home">
+          <div className="text-box">
+            <h2>Hello, I am</h2>
+            <h1>Soumen Dass</h1>
+            <p>Aspiring Web Developer | Exploring AI & Backend</p>
+          </div>
+
+          <div className="image">
+            <img src="src/assets/portfoliioImage.png" alt="Main Image" />
+          </div>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default profile
+export default Profile;
