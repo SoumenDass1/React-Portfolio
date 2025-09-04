5f25beb8d1b07efad10b0947d404d0a694b68a7c
SoumenDass2006 <soumendass1357@gmail.com>
2025-09-04 09:43:38 +0530

    Home page is completed. Now working on about page

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,42 +1,54 @@
-import React from "react";
-import "../css/profile.css";
+import "../css/header.css";
+import "../css/home.css";
+import "../css/about.css";
 
 function Profile() {
-  return (
-    <>
-      <header>
-        <div className="container">
-          <div className="logo-box">
-            <h1 className="logo">Portfolio</h1>
-          </div>
+    return (
+        <>
+            <header>
+                <div className="container">
+                    <div className="logo-box">
+                        <h1 className="logo">Portfolio</h1>
+                    </div>
 
-          <nav className="nav-bar">
-            <ul>
-              <li><a href="/">Home</a></li>
-              <li><a href="/about">About</a></li>
-              <li><a href="/skills">Skills</a></li>
-              <li><a href="/projects">Projects</a></li>
-              <li><a href="/contact">Contact</a></li>
-            </ul>
-          </nav>
-        </div>
-      </header>
+                    <nav className="nav-bar">
+                        <ul>
+                            <li><a href="/home">Home</a></li>
+                            <li><a href="/about">About</a></li>
+                            <li><a href="/skills">Skills</a></li>
+                            <li><a href="/projects">Projects</a></li>
+                            <li><a href="/contact">Contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
 
-      <main>
-        <section className="home">
-          <div className="text-box">
-            <h2>Hello, I am</h2>
-            <h1>Soumen Dass</h1>
-            <p>Aspiring Web Developer | Exploring AI & Backend</p>
-          </div>
+            <main>
+                <section id="home" className="home">
+                    <div className="text-box">
+                        <h2>Hello, I am</h2>
+                        <h1>Soumen Dass</h1>
+                        <p>Aspiring Web Developer | Exploring AI & Backend</p>
+                    </div>
 
-          <div className="image">
-            <img src="src/assets/portfoliioImage.png" alt="Main Image" />
-          </div>
-        </section>
-      </main>
-    </>
-  );
+                    <div className="image">
+                        <img src="src/assets/portfoliioImage.png" alt="Main Image" />
+                    </div>
+                </section>
+
+                <section id="about" className="about">
+                    <div className="about-box">
+                        <h1>About Me</h1>
+                        <div className="about-me">
+                            <p>I am an aspiring web developer exploring AI & Backend.</p>
+                        </div>
+                    </div>
+                </section>
+
+            </main>
+        </>
+    );
 }
+
 
 export default Profile;
